7a3983db8b4efb7b6ac4bdbe1ab137619d38b44e
Oliver276A <kcumud@gmail.com>
2025-05-27 21:58:01 -0400

    added turning on and off mechanism

--- ttpt.ino
+++ ttpt.ino
@@ -3,8 +3,8 @@
 // Pin Assignment
 const int echoPin = 5;
 const int trigPin = 6;
-const int wheelPin = 11;
-const int stickPin = 9;
+const int wheelPin = 9;
+const int stickPin = 11;
 
 // Servo objects
 Servo stick;
@@ -13,6 +13,7 @@ Servo wheel;
 // Variables
 long duration;
 int distance;
+bool robotOn = false;
 
 void setup() {
 
@@ -21,18 +22,56 @@ void setup() {
   wheel.attach(wheelPin);
   // Clearing the servos
   stick.write(0);
-  wheel.write(0);
+  wheel.write(90);
 
   // Ultrasonic sensor setup
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 
+    // First wait for signal to start
+  int i = 0;
+  while(i < 6) {
+    getDistance();
+    if (distance < 5) {
+      i ++;
+      delay(500);
+    }
+  }
+  robotOn = true;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // Actual Loop
+  getDistance();
+  Serial.println(distance);
+
+
+
+  // Turning off mechanism
+  if ((distance < 5) and (robotOn = true)) { // If very close, trigger the mechanism
+
+    int j = 0; // Make an integer
+
+    getDistance(); // Get new distance
+    while(j < 6) {
+      if (distance <= 5) { // Very close
+        j ++; // Count six consequetive times (3 seconds)
+        delay(500);
+      }
+      else {break;} // If far again, proceed
+    }
+
+    if (j >= 6) {
+      while(true) {} //If more than 3 seconds, shut down
+    }
+  }
+  
+  delay(1000);
+}
+
+void getDistance() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -40,7 +79,4 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration * 0.034) / 2;
-
-  Serial.println(distance);
-  delay(1000);
 }
